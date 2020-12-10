9b41a807059caf4b2839f46af8a552509570130c
Prince-12345 <princebhandari67@gmail.com>
2020-12-10 19:06:50 +0530

    Class31

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
 
+var gameState = "onSling";
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -65,20 +66,25 @@ function draw(){
     bird.display();
     platform.display();
     //log6.display();
-    slingshot.display();    
+    slingshot.display(); 
+    
+    text(mouseX+" , "+mouseY , mouseX,mouseY)
 }
 
 function mouseDragged(){
+    if(gameState !== "launched"){
     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
 }
 
 
 function mouseReleased(){
+    gameState = "launched";
     slingshot.fly();
 }
 
-function keyPressed(){
-    if(keyCode === 32){
-        slingshot.attach(bird.body);
-    }
-}
+//function keyPressed(){
+  //  if(keyCode === 32){
+    //    slingshot.attach(bird.body);
+   // }
+//}
